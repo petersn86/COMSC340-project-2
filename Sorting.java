61858c8182976a0d6632281deda5ce045bab1e66
petersn86 <133061453+petersn86@users.noreply.github.com>
2024-04-08 16:48:39 -0400

    changes

--- Sorting.java
+++ Sorting.java
@@ -1,40 +1,69 @@
 import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Sorting
 {
     void insertionsort(int n, File file)  throws IOException{
 
-        FileReader fr = new FileReader(file);
-        BufferedReader br = new BufferedReader(fr);
+        FileReader fr = new FileReader(file); //create file reader
+        BufferedReader br = new BufferedReader(fr); //create a buffer reader
 
-        int[] S = new int[n];
-        int m = 0;
+        int[] S = new int[n]; //n is size of file (# of lines)
+        int m = 0; 
         for (String line = br.readLine(); line != null; line = br.readLine()) {
-            S[m] = Integer.parseInt(line);
+            S[m] = Integer.parseInt(line.replace(" ", "")); //store each line into an array
             m++;
         }
 
-        br.close();
+        br.close(); //close buffer reader
 
-        for(int i = 1; i <= n; i++){
+        for (int i = 1; i < n; i++) { // Corrected loop condition
             int x = S[i];
             int j = i - 1;
-            while(j > 0 && S[j] > x){
+            while (j >= 0 && S[j] > x) { // Corrected loop condition
                 S[j + 1] = S[j];
-                j --;
+                j--;
             }
             S[j + 1] = x;
         }
+
+        FileWriter fw = new FileWriter(file); //create file writer
+        BufferedWriter bw = new BufferedWriter(fw); //create buffer writer
+
+        for (int i = 0; i < n; i++) { //overwrite lines with sorted values
+            bw.write(Integer.toString(S[i]));
+            bw.newLine();
+        }
+
+        bw.close(); //close buffer writer
     }
 
-    
+    public static void main(String[] args) throws IOException{
+        File file = new File("test.txt"); 
 
+        FileReader reader = new FileReader(file);
+        BufferedReader bReader = new BufferedReader(reader);
+        int lines = 0;
+        while (bReader.readLine() != null){
+            lines++;
+        }
+        reader.close();
 
+        Sorting sorter = new Sorting();
+        sorter.insertionsort(lines, file);
 
-    public static void main(String[] args){
+        FileReader sortedReader = new FileReader(file);
+        BufferedReader sortedBReader = new BufferedReader(sortedReader);
+        String line;
+        while ((line = sortedBReader.readLine()) != null) {
+            System.out.println(line);
+        }
+        sortedReader.close();
+
     }
 
 }
