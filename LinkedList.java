b4685b371dff4f4426e58b04d4321f821f5fb61a
petersn86 <133061453+petersn86@users.noreply.github.com>
2024-04-08 17:20:53 -0400

    changes

---
+++ LinkedList.java
@@ -0,0 +1,48 @@
+public class LinkedList {
+    public Node head;
+
+    public LinkedList() {
+        this.head = null;
+    }
+
+    public void append(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    public void printList() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+    
+    public int getElementAt(int index) {
+        if (head == null || index < 0) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+
+        Node current = head;
+        int currentIndex = 0;
+
+        while (current != null) {
+            if (currentIndex == index) {
+                return current.data;
+            }
+            current = current.next;
+            currentIndex++;
+        }
+
+        throw new IndexOutOfBoundsException("Index out of bounds");
+    }
+}
--- Sorting.java
+++ Sorting.java
@@ -5,9 +5,8 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class Sorting
-{
-    void insertionsort(int n, File file)  throws IOException{
+public class Sorting{
+    void insertionsortArray(int n, File file)  throws IOException{
 
         FileReader fr = new FileReader(file); //create file reader
         BufferedReader br = new BufferedReader(fr); //create a buffer reader
@@ -21,10 +20,10 @@ public class Sorting
 
         br.close(); //close buffer reader
 
-        for (int i = 1; i < n; i++) { // Corrected loop condition
+        for (int i = 1; i < n; i++) { //implementation of insertion sort
             int x = S[i];
             int j = i - 1;
-            while (j >= 0 && S[j] > x) { // Corrected loop condition
+            while (j >= 0 && S[j] > x) { 
                 S[j + 1] = S[j];
                 j--;
             }
@@ -42,6 +41,55 @@ public class Sorting
         bw.close(); //close buffer writer
     }
 
+    void insertionsortLinkedList(int n, File file) throws IOException{
+        FileReader fr = new FileReader(file); //create file reader
+        BufferedReader br = new BufferedReader(fr); //create a buffer reader
+
+        LinkedList S = new LinkedList();
+        for (String line = br.readLine(); line != null; line = br.readLine()) {
+            S.append(Integer.parseInt(line.replace(" ", ""))); //store each line into linked list
+        }
+
+        br.close(); //close buffer reader
+
+        Node current = S.head.next; // Start from the second node
+        while (current != null) {
+            int key = current.data;
+            Node prev = current;
+            Node temp = S.head;
+    
+            // Find the correct position to insert the current node
+            while (temp != current && temp.data < key) {
+                prev = temp;
+                temp = temp.next;
+            }
+    
+            if (temp != current) { // If the correct position is not the current position
+                prev.next = current.next;
+                current.next = temp;
+                if (temp == S.head) {
+                    S.head = current;
+                } else {
+                    prev.next = current;
+                }
+            } else { // If the correct position is the current position
+                current = current.next;
+            }
+        }
+        
+        FileWriter fw = new FileWriter(file); //create file writer
+        BufferedWriter bw = new BufferedWriter(fw); //create buffer writer
+
+        for (int i = 0; i < n; i++) { //overwrite lines with sorted values
+            bw.write(Integer.toString(S.getElementAt(i)));
+            bw.newLine();
+        }
+
+        bw.close(); //close buffer writer
+
+    }
+    
+
     public static void main(String[] args) throws IOException{
         File file = new File("test.txt"); 
 
@@ -54,7 +102,7 @@ public class Sorting
         reader.close();
 
         Sorting sorter = new Sorting();
-        sorter.insertionsort(lines, file);
+        sorter.insertionsortLinkedList(lines, file);
 
         FileReader sortedReader = new FileReader(file);
         BufferedReader sortedBReader = new BufferedReader(sortedReader);
